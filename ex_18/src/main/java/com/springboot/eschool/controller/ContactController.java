92ff9b3eeaf8e874efae150d0cd7fa505e8651ff
Sourabh dubey <dubeysourabh2001@gmail.com>
2023-06-07 18:43:56 +0530

    Validation using annotation

--- ex_18/src/main/java/com/springboot/eschool/controller/ContactController.java
+++ ex_18/src/main/java/com/springboot/eschool/controller/ContactController.java
@@ -2,16 +2,21 @@ package com.springboot.eschool.controller;
 
 import com.springboot.eschool.model.Contact;
 import com.springboot.eschool.service.ContactService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@Slf4j
 @Controller
 public class ContactController {
 
@@ -23,13 +28,18 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContact(){
+    public String displayContact(Model model){
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
     @RequestMapping(value="/saveMsg", method = POST)
-    public ModelAndView saveMessage(Contact contact){
+    public String saveMessage(@ModelAttribute("contact") Contact contact, Errors errors){
+        if(errors.hasErrors()){
+            log.error("Contact form validation restricted due to : " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageService(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
--- ex_18/src/main/java/com/springboot/eschool/model/Contact.java
+++ ex_18/src/main/java/com/springboot/eschool/model/Contact.java
@@ -1,14 +1,32 @@
 package com.springboot.eschool.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Contact {
 
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 3, message = "Name must be at least 3 character long")
     private String name;
+
+    @NotBlank(message = "Mobile Number must not be blank")
+    @Pattern(regexp = "(^$[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNum;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Please Provide a valid Email")
     private String email;
+
+    @NotBlank(message = "Subject must not be blank")
+    @Size(min = 5, message = "Subject must be at least 5 character long")
     private String subject;
+
+    @NotBlank(message = "Message must not be blank")
+    @Size(min = 10, message = "Message must be at least 10 charcter long")
     private String message;
 
 }
