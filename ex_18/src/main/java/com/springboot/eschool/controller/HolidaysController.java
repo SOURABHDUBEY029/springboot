4968b9c5d278d2bb3670633c839ba80759e33b4a
Sourabh dubey <dubeysourabh2001@gmail.com>
2023-06-07 15:13:02 +0530

    Path variable

--- ex_18/src/main/java/com/springboot/eschool/controller/HolidaysController.java
+++ ex_18/src/main/java/com/springboot/eschool/controller/HolidaysController.java
@@ -4,6 +4,7 @@ import com.springboot.eschool.model.Holiday;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
@@ -13,11 +14,19 @@ import java.util.stream.Collectors;
 @Controller
 public class HolidaysController {
 
-    @GetMapping("/holidays")
-    public String displayHolidays(@RequestParam(required = false) boolean festival,
-                                  @RequestParam(required = false) boolean federal, Model model){
-        model.addAttribute("festival", festival);
-        model.addAttribute("federal", federal);
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display, Model model){
+        if(display != null && display.equals("all")) {
+            model.addAttribute("festival", true);
+            model.addAttribute("federal", true);
+        }
+        if(display != null && display.equals("festival")) {
+            model.addAttribute("festival", true);
+        }
+        if(display != null && display.equals("federal")) {
+            model.addAttribute("federal", true);
+        }
+
         List<Holiday> holidays = Arrays.asList(
                 new Holiday("January 1", "New Year", Holiday.Type.FESTIVAL),
                 new Holiday("January 26", "Republic Day", Holiday.Type.FEDERAL),
