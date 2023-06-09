cc0731686855d415f9f2712c2d95b720e90ddb02
Sourabh dubey <dubeysourabh2001@gmail.com>
2023-06-09 13:20:02 +0530

    Spring Security & Testing with default credential

--- ex_18/src/main/java/com/springboot/eschool/service/ContactService.java
+++ ex_18/src/main/java/com/springboot/eschool/service/ContactService.java
@@ -13,7 +13,7 @@ import org.springframework.web.context.annotation.SessionScope;
 @Service
 //@RequestScope
 //@SessionScope
-@ApplicationScope
+//@ApplicationScope
 public class ContactService {
 
 //    private static Logger log = LoggerFactory.getLogger(ContactService.class);
