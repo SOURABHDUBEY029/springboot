5887f010e58f8bf7fd14baacf6d161952b2dc619
Sourabh dubey <dubeysourabh2001@gmail.com>
2023-06-13 18:02:01 +0530

    Implement error page using ControllerAdvice Annotation

--- ex_18/src/main/java/com/springboot/eschool/controller/DashboardController.java
+++ ex_18/src/main/java/com/springboot/eschool/controller/DashboardController.java
@@ -15,6 +15,7 @@ public class DashboardController {
     public String displayDashboard(Model model, Authentication authentication){
         model.addAttribute("username", authentication.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+//        throw new RuntimeException("It is runtime exception !!");
 
         return "dashboard.html";
     }
